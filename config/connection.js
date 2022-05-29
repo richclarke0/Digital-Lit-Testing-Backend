6146ba8ae9b5c3742949547a4e3f300587c75c68
Richard Clarke <freshnewrich@gmail.com>
2022-05-28 19:14:01 -0700

    connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const cowsay = require("cowsay")
 
 mongoose.connect(process.env.DATABASE_URL);
 let dbase = mongoose.connection
-dbase.on("open", () => console.log(cowsay.say({text: `Mongo run moo db:${dbase.host}:${dbase.port}`})))
+dbase.on("open", () => console.log(`Mongo run moo db:${dbase.host}:${dbase.port}`))
 .on("close", () => console.log(cowsay.say({text: "Mongo broke moo"})))
 .on("error", (err) => console.log(cowsay.say({text: `Mongo error moo --> ${error}`})))
 
