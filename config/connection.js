a3790abc0f34af9491f507c1817a6ebf00c42822
Richard Clarke <freshnewrich@gmail.com>
2022-05-28 19:41:35 -0700

    dbase url thing

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.DATABASE_URL);
+let DATABASE_URL = process.env.DATABASE_URL
+
+mongoose.connect(DATABASE_URL);
 let dbase = mongoose.connection
 dbase.on("open", () => console.log(`Mongo run db:${dbase.host}:${dbase.port}`))
 .on("close", () => console.log("Mongo close"))
