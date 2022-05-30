57a3416d2540b4532ab5b9e849ab8b00b2b651da
Richard Clarke <freshnewrich@gmail.com>
2022-05-30 10:29:58 -0700

    change question model

--- models/question.js
+++ models/question.js
@@ -7,8 +7,9 @@ const questionSchema = new mongoose.Schema({
         enum: ["multi","text"]
     },
     question: {type : String},
-    multis: [{type : String}],
+    multis: [{type : String}], //restrings the array to strings
     img : {type: String},
+    answer: {type: String}
 });
 
 module.exports = mongoose.model('Question', questionSchema);
