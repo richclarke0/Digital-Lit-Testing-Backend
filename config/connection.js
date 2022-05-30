24cdffd6b5c3676e69856dd7e519acd4204b6535
Richard Clarke <freshnewrich@gmail.com>
2022-05-30 10:33:01 -0700

    connection typo

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ mongoose.connect(DATABASE_URL);
 let dbase = mongoose.connection
 dbase.on("open", () => console.log(`Mongo run db:${dbase.host}:${dbase.port}`))
 .on("close", () => console.log("Mongo close"))
-.on("error", (err) => console.log(`Mongo error --> ${error}`))
+.on("error", (err) => console.log(`Mongo error --> ${err}`))
 
 
 
